552d02fb0422d2ea84693dbc92d8a986bba30ef3
leomarx <suporte@leomarx.com>
2022-02-22 03:21:12 -0300

    #7 - Labs TerraForm VPC rename, S3 storage, egress, depends_on

--- 2_terraform/0_labs/main.tf
+++ 2_terraform/0_labs/main.tf
@@ -16,11 +16,13 @@ resource "aws_instance" "labs-alura" {
   tags = {
     Name = "lab${count.index}"
   }
+  #vpc_security_group_ids = ["sg-01b768ceef80390a0", "sg-059e6f0d6624bc548"]
+  vpc_security_group_ids = ["${aws_security_group.inbound-ssh-access.id}", "${aws_security_group.outbound-http-update-ubuntu.id}"]
 }
 
-resource "aws_security_group" "ssh-access" {
-  name = "ssh-config"
-  description = "ssh-config"
+resource "aws_security_group" "inbound-ssh-access" {
+  name = "inbound-ssh-config"
+  description = "inbound-ssh-config"
 
   ingress {
     from_port = 22
@@ -29,6 +31,34 @@ resource "aws_security_group" "ssh-access" {
     cidr_blocks = ["177.102.167.65/32"]
   }
   tags = {
+    Name = "inbound"
     Name = "ssh"
+  }
+}
+
+resource "aws_security_group" "outbound-http-update-ubuntu" {
+  name = "outbound-http-config"
+  description = "outbound-http-config"
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["91.189.88.142/32"]
+  }
+
+  tags = {
+    Name = "outbound"
+    Name = "http"
+  }
+  depends_on = [aws_security_group.inbound-ssh-access]
+}
+
+resource "aws_s3_bucket" "lab-storage" {
+  bucket = "lab-storage-nfs"
+  #deprecated
+  #acl = "private"
+  tags = {
+    Name = "storage"
   }
 }
